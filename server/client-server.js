60834959c0fe256eac0a98869404a8187c20fd24
jyek <justinyek@gmail.com>
2014-03-18 21:02:26 -0700

    Changed IP address and ports for server

--- server/client-server.js
+++ server/client-server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var server = express(); 
+var server = express();
 server.configure(function(){
   var httpServerRoot = __dirname + '/../client/';
   console.log(httpServerRoot);
   server.use('/', express.static(httpServerRoot));
 });
 
-server.listen(8000);
+server.listen(3001, '23.97.67.147');
